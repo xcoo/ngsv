f843d9d40ee78215edf740ab4e5982c99b07f697
Toshiki TAKEUCHI <take@xcoo.jp>
2012-12-21 17:14:38 +0900

    Fix a bug about setup of cypileup

--- tools/cypileup/setup.py
+++ tools/cypileup/setup.py
@@ -2,10 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension('ngsv.cypileup', ['cypileup.pyx'])]
+ext_module = Extension('ngsv.cypileup', [ 'ngsv/cypileup.pyx' ])
 
 setup(
   name = 'ngsv cypileup',
+  version = '0.1.0',
+  description = 'ngsv cypileup',
+  url = 'http://www.xcoo.jp/',
+  author = 'Xcoo, Inc.',
+  ext_modules = [ ext_module ],
   cmdclass = { 'build_ext': build_ext },
-  ext_modules = ext_modules
+  packages = [ 'ngsv' ]
 )
